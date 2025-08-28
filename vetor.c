2dc86fe687a6000e8d35eec341f753ada35ed44f
joaofstival <jstival99@gmail.com>
2025-08-28 01:41:45 +0000

    Criado o arquivo que encontra o maior valor do vetor

---
+++ vetor.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int vetor[10];
+    int i;
+    int maior;
+
+    printf("Digite 10 numeros inteiros:\n");
+    for(i = 0; i < 10; i++) {
+        printf("Valor %d: ", i + 1);
+        scanf("%d", &vetor[i]);
+    }
+
+    maior = vetor[0];
+
+    for(i = 1; i < 10; i++) {
+        if(vetor[i] > maior) {
+            maior = vetor[i];
+        }
+    }
+
+    printf("\nO maior valor digitado foi: %d\n", maior);
+
+    return 0;
+}
